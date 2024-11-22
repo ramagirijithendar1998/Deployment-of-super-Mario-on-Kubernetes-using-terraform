21a33f909ccbc8f6a3ef18fd43d3e07870a04b24
ramagirijithendar1998 <jithendarramagiri1998@gmail.com>
2024-11-22 16:25:48 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "jeethu" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
